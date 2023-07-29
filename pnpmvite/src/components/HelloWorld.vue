65a58254878a2e0bab75be4d0eb30a1dab87a69c
ilovexgq <42941529+ilovexgq@users.noreply.github.com>
2023-07-29 14:45:33 +0800

    重置样式
    npm install normalize.css
    import 'normalize.css'

--- pnpmvite/src/components/HelloWorld.vue
+++ pnpmvite/src/components/HelloWorld.vue
@@ -32,7 +32,5 @@ const count = ref(0)
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
--- pnpmvite/src/main.ts
+++ pnpmvite/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import 'normalize.css'
+
 import './style.css'
 import App from './App.vue'
 
